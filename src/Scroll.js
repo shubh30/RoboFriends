b84555d4a5caf1974e6e10a061794a3e5b7d4efe
Shubham <shubhamnagpal30@gmail.com>
2020-06-30 18:37:49 +0530

    Add Scroll Component with styles

--- src/Scroll.js
+++ src/Scroll.js
@@ -4,7 +4,7 @@ import './Scroll.css'
 const Scroll = (props) => {
   return (
     <div id='cont-1'>
-      <div id='cont-2'>
+      <div id='cont-2' className='example'>
         { props.children }
       </div>
     </div>
